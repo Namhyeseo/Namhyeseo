5ad69865a45634296fd3c2e96f280bdbce2f584b
Namhyeseo <skagptj123@naver.com>
2024-11-12 17:19:36 +0900

    ~11/11

--- portpolio/08-template/js/script.js
+++ portpolio/08-template/js/script.js
@@ -1,5 +1,5 @@
 //서브메뉴 호버시 0.5초마다 올라갔다 내려감
-$('.navi>li').mouseover(function() {
+$('#gnb>li').mouseover(function() {
     $(this).find('.submenu').stop().slideDown(500);
 }).mouseout(function(){
     $(this).find('.submenu').stop().slideUp(500);
